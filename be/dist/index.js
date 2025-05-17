deee06890da5ba694c4b72810824f0ddb6158c9f
ParasRana123 <parasrana579@gmail.com>
2025-05-17 23:34:06 +0530

    Integrated Gemini model into the poject

--- be/dist/index.js
+++ be/dist/index.js
@@ -1,3 +1,29 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
-console.log(process.env.GEMINI_API_KEY);
+const generative_ai_1 = require("@google/generative-ai");
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY || "";
+const ai = new generative_ai_1.GoogleGenerativeAI(GEMINI_API_KEY);
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const model = ai.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const chat = model.startChat({
+            history: [
+                { role: "user", parts: [{ text: "What is Machine Learning" }] }
+            ]
+        });
+        const result = yield chat.sendMessage("Summarize everything in one sentence.");
+        const response = yield result.response;
+        console.log(response.text());
+    });
+}
+main();
--- be/src/index.ts
+++ be/src/index.ts
@@ -1,4 +1,23 @@
 require("dotenv").config();
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-console.log(process.env.GEMINI_API_KEY);
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY || "";
 
+const ai = new GoogleGenerativeAI(GEMINI_API_KEY);
+
+async function main() {
+  const model = ai.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const chat = model.startChat({
+    history: [
+      { role: "user", parts: [{ text: "What is Machine Learning" }] }
+    ]
+  });
+
+  const result = await chat.sendMessage("Summarize everything in one sentence.");
+  const response = await result.response;
+
+  console.log(response.text());
+}
+
+main();
